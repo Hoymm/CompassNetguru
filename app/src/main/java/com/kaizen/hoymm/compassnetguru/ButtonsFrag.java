6e44aec0e5079ccf304ce8a84ef03edd5e0800b2
Hoymm <Hoymm241@wp.pl>
2018-02-05 14:05:06 +0100

    Add buttons fragment

--- app/src/main/java/com/kaizen/hoymm/compassnetguru/ButtonsFrag.java
+++ app/src/main/java/com/kaizen/hoymm/compassnetguru/ButtonsFrag.java
@@ -6,23 +6,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
 /**
  * Created by Damian Muca (Kaizen) on 05.02.18.
  */
 
-public class CompassFrag extends Fragment {
-    private ImageView pointer;
-
+public class ButtonsFrag extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.compass_frag, container, false);
-    }
-
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
+        return inflater.inflate(R.layout.buttons_frag, container, false);
     }
 }
--- app/src/main/java/com/kaizen/hoymm/compassnetguru/MainActivity.java
+++ app/src/main/java/com/kaizen/hoymm/compassnetguru/MainActivity.java
@@ -5,16 +5,28 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     private CompassFrag compassFrag;
+    private ButtonsFrag buttonsFrag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        addCompassFrag();
+        initAndAddFragments();
     }
 
-    private void addCompassFrag() {
+    private void initAndAddFragments() {
+        initAndAddCompassFrag();
+        initAndAddButtonsFrag();
+    }
+
+    private void initAndAddButtonsFrag() {
+        buttonsFrag = new ButtonsFrag();
+        getSupportFragmentManager().beginTransaction().add(R.id.buttonsFrame, buttonsFrag).commit();
+    }
+
+    private void initAndAddCompassFrag() {
         compassFrag = new CompassFrag();
         getSupportFragmentManager().beginTransaction().add(R.id.compassFrame, compassFrag).commit();
     }
+
 }
