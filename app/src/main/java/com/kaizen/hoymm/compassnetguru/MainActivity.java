29a175f32f1ca7a771238e296a2693cb7ec9cddd
Hoymm <Hoymm241@wp.pl>
2018-02-05 16:01:03 +0100

    Make app fullscreen and hide status bar

--- app/src/main/java/com/kaizen/hoymm/compassnetguru/MainActivity.java
+++ app/src/main/java/com/kaizen/hoymm/compassnetguru/MainActivity.java
@@ -1,8 +1,10 @@
 package com.kaizen.hoymm.compassnetguru;
 
+import android.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     private CompassFrag compassFrag;
@@ -11,8 +13,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        hideStatusBar();
         setContentView(R.layout.activity_main);
         initAndAddFragments();
+    }
+
+    private void hideStatusBar() {
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
+        ActionBar actionBar = getActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
     }
 
     private void initAndAddFragments() {
